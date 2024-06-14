777589b18ef60c075ee983572c546578dc87b701
Curious-Goblin <poddarsourabh9939@gmail.com>
2024-06-14 18:57:36 +0530

    added error check in sign up and sign in components

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -2,16 +2,26 @@ import { Auth } from "../components/Auth"
 import { Quote } from "../components/Quote"
 
 export const Signup = () => {
-    return (
-        <>
-            <div className="grid grid-cols-1 lg:grid-cols-2">
-                <div>
-                    <Auth type="signup"/>
+    try {
+        return (
+            <>
+                <div className="grid grid-cols-1 lg:grid-cols-2">
+                    <div>
+                        <Auth type="signup" />
+                    </div>
+                    <div className="hidden lg:block">
+                        <Quote />
+                    </div>
                 </div>
-                <div className="hidden lg:block">
-                    <Quote />
-                </div>
+            </>
+        )
+    }
+    catch (error) {
+        alert(console.log(error))
+        return (
+            <div>
+                Some error has occured please check you input format
             </div>
-        </>
-    )
+        )
+    }
 }
